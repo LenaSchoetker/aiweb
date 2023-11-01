beb5bc9c5718b3fdaa9f7d7a0f1a21f39e2ca36b
m-buschmann <snep114@gmail.com>
2023-11-01 14:47:03 +0100

    update

--- trial.py
+++ trial.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-def is_internal_link(url, base_url):
+"""def is_internal_link(url, base_url):
     # Check if the URL is an internal link (within the same server)
     # Returns True or False
-    return base_url in url
+    return base_url in url"""
 
 def get_internal_links(soup, base_url):
     # Find and return all internal links on the page
     internal_links = []
     for link in soup.find_all("a"):
         href = link.get("href")
-        if href and is_internal_link(href, base_url):
+        if href and (base_url in href): #is_internal_link(href, base_url):
             internal_links.append(href)
     return internal_links
 
@@ -30,24 +30,25 @@ def crawler(start_url, base_url):
         if response.status_code == 200 and 'text/html' in response.headers.get('Content-Type', ''):
             soup = BeautifulSoup(response.content, 'html.parser')
 
-        #get content
-        title_tag = soup.title
-        if title_tag:
-            print("Title:", title_tag.text)            
-            #title_tag.text #text content of a Tag
+            #get content
+            title_tag = soup.title
+            if title_tag:
+                print("Title:", title_tag.text)            
+                #title_tag.text #text content of a Tag
 
-        #analyze page, update index
+            #analyze page, update index
 
-        # update visited list
-        visited.add(current_url)
+            # update visited list
+            visited.add(current_url)
 
-        # Find and add internal links to the stack
-        internal_links = get_internal_links(soup, base_url)
-        for link in internal_links:
-            stack.append(link)
+            # Find and add internal links to the stack
+            internal_links = get_internal_links(soup, base_url)
+            for link in internal_links:
+                stack.append(link)
 
-    else:
-        print("Error: Failed to fetch the page. Status code:", r.status_code)
+        else:
+            print(f"Error: Failed to fetch the page. Status code: {response.status_code}, URL: {current_url}")
+
 
 # Test with given URL
 start_url = "https://vm009.rz.uos.de/crawl/index.html"
