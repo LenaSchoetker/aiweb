37498041f1714520a2c1eece8c3afd6bde22f7c6
m-buschmann <snep114@gmail.com>
2023-11-01 14:36:54 +0100

    update

--- trial.py
+++ trial.py
@@ -15,45 +15,41 @@ def get_internal_links(soup, base_url):
             internal_links.append(href)
     return internal_links
 
-def crawler(start_url):
+def crawler(start_url, base_url):
     visited = set()  # Set to keep track of visited pages
     stack = [start_url]  # Stack to store links to visit
 
-    if start.status_code == 200:
+    while stack: #links available on this page or more branches available to follow
+        current_url = stack.pop()
 
-        while stack: #links available on this page or more branches available to follow
-            current_url = stack.pop()
+        # If page visited before, continue
+        if current_url in visited:
+            continue
 
-            #if page visited before, continue:
-            if current_url in visited:
-                continue
-
-            response = requests.get(current_url)
+        response = requests.get(current_url)
+        if response.status_code == 200 and 'text/html' in response.headers.get('Content-Type', ''):
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            #get content
-            title_tag = soup.title
-            if title_tag:
-                print("Title:", title_tag.text)            
-                #title_tag.text #text content of a Tag
+        #get content
+        title_tag = soup.title
+        if title_tag:
+            print("Title:", title_tag.text)            
+            #title_tag.text #text content of a Tag
 
-            #analyze page, update index
-            # List of links on that website
-            for l in soup.find_all("a"):
-                print(l)
-                # add links to the stack (push)
+        #analyze page, update index
 
-            # update visited list
-            visited.add(current_url)
+        # update visited list
+        visited.add(current_url)
 
-            # Find and add internal links to the stack
-            base_url = start_url.split('/')[2]
-            internal_links = get_internal_links(soup, base_url)
-            for link in internal_links:
-                stack.append(link)
+        # Find and add internal links to the stack
+        internal_links = get_internal_links(soup, base_url)
+        for link in internal_links:
+            stack.append(link)
 
     else:
         print("Error: Failed to fetch the page. Status code:", r.status_code)
 
-start = requests.get("https://vm009.rz.uos.de/crawl/index.html")
-crawler(start)
+# Test with given URL
+start_url = "https://vm009.rz.uos.de/crawl/index.html"
+base_url = start_url.split('/')[2]
+crawler(start_url, base_url)
