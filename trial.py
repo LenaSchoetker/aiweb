42a1bffaeda5c88d5f06d8529b2f664edacd1c14
m-buschmann <snep114@gmail.com>
2023-11-01 13:48:36 +0100

    update

--- trial.py
+++ trial.py
@@ -1,19 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-x = True
+def crawler(start_url):
+    visited = set()  # Set to keep track of visited pages
+    stack = [start_url]  # Stack to store links to visit
 
-def crawler(r):
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
+    if start.status_code == 200:
 
-        while x:#links available on this page or more branches available to follow
-            #if page not visited before:
-                # List of links on that website
-                for l in soup.find_all("a"):
-                    print(l)
+        while stack: #links available on this page or more branches available to follow
+            current_url = stack.pop()
+
+            #if page visited before, continue:
+            if current_url in visited:
+                continue
+
+            response = requests.get(current_url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            #get content
+            title_tag = soup.title
+            if title_tag:
+                print("Title:", title_tag.text)            
+                #title_tag.text #text content of a Tag
+
+            #analyze page, update index
+            # List of links on that website
+            for l in soup.find_all("a"):
+                print(l)
+                # add links to the stack (push)
+
+            # update visited list
+            visited.add(current_url)
     else:
         print("Error: Failed to fetch the page. Status code:", r.status_code)
 
-r = requests.get("https://vm009.rz.uos.de/crawl/index.html")
-crawler(r)
+start = requests.get("https://vm009.rz.uos.de/crawl/index.html")
+crawler(start)
